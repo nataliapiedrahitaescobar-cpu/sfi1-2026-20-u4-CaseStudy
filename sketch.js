6c90187ef24fbc38fb3f7991d0d758fb12648c4a
Natalia Piedrahita <nataliapiedrahitaescobar@gmail.com>
2026-03-11 09:38:11 -0500

    Código sketch corregido

--- sketch.js
+++ sketch.js
@@ -156,64 +156,40 @@ function draw() {
     renderer.get(painter.state)?.();
 }
 
-function drawRunning() {
-   // Copy from P_2_0_02
-//
-// Generative Gestaltung – Creative Coding im Web
-// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
-// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
-// with contributions by Joey Lee and Niels Poldervaart
-// Copyright 2018
-//
-// http://www.generative-gestaltung.de
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+function drawRunning() { //Ejecuta cada frame mientras la máquina de estados esté en estado_corriendo.
+   let mb = painter.rexData;
 
-function setup() {
-    createCanvas(720, 720);
+   if (!mb.ready) return;
+
+   push();
+   translate(width / 2, height / 2);
+
+   //Resolución del polígono según inclinación en y
+   let circleResolution = int(map(mb.y, 0, height, 2, 10));
+
+   //Radio según resolución en x
+   let radius = mb.x - width/2;
+
+   let angle = TAU / circleResolution;
+
+   //Botón b activa relleno
+   if(mb.btnB){
+    fill(34,45,122,50);
+   }else {
     noFill();
-    background(255);
-    strokeWeight(2);
-    stroke(0, 25);
-}
+   }
 
-function draw() {
-    if (mb.btnA) {
-        push();
-        translate(width / 2, height / 2);
+   beginShape();
 
-        // Debe mapearse al eje Y del acelerómetro.
-        let circleResolution = int(map(mb.y,-2048, 2047,2, 10));
+   for (let i = 0; i <= circleResolution; i++){
+    let x = cos(angle * i) * radius;
+    let y = sin(angle * i) * radius;
+    vertex(x,y);
+   }
 
-        // Debe mapearse al eje X del acelerómetro.
-        let radius = map(mb.x,-2048, 2047,-width/2, width/2);
-        let angle = TAU / circleResolution;
+   endShape();
 
-        // Debe ser activado por el botón B del dispositivo.
-        if(mb.btnB) {
-        fill(34, 45, 122, 50);
-        } else {
-        noFill();
-        }
-
-        beginShape();
-        for (let i = 0; i <= circleResolution; i++) {
-        let x = cos(angle * i) * radius;
-        let y = sin(angle * i) * radius;
-        vertex(x, y);
-        }
-        endShape();
-
-        pop();
-    }
-}
+   pop();
 }
 
 function windowResized() {
