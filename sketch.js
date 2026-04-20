bc15bb6262959dc673ff0574ee4092dc58c86497
Natalia Piedrahita <nataliapiedrahitaescobar@gmail.com>
2026-04-20 18:52:28 -0500

    Listo sketch

--- sketch.js
+++ sketch.js
@@ -69,49 +69,60 @@ function draw() {
 }
 
 function drawRunning() {
-    background(0, 40);
-
-    //Activar eventos en el tiempo correcto
+ //Activar eventos en el tiempo correcto
     painter.processEvents();
 
+    let mb = painter.rxData;
+    if (!mb || !mb.ready) return;
+
+    let now = Date.now();
+
     //Dibujo de Strudel
-    for (let anim of painter.ActiveAnimations) {
-
-        let now = Date.now(); //Tiempo actual para calcular la animación.
+    for (let i = painter.activeAnimations.length -1;  i >= 0; i--) {
+ 
+        let anim = painter.activeAnimations[i];
         let progress = (now - anim.startTime) / anim.duration; //Progeso de la animación.
-
-        if (progress > 1) continue; //Si la animación ha terminado, saltar a la siguiente.
-
-        switch (anim.type) {
+        
+    //Eliminar animaciones que han terminado
+        if (progress > 1) {
             
-            case "tr909bd": //Bombo
-            fill (255, 0, 80);
-            noStroke();
-            circle(width / 2, height / 2, lerp(50, 300, progress)); //Dibuja un círculo que crece con el tiempo.
-            break;
-
-            case "tr909sd": //Caja
-            fill (0, 200, 255);
-            rectMode(CENTER); 
-            rect(width / 2, height / 2, lerp(width, 0, progress), 50); //Dibuja un rectángulo que se encoge con el tiempo.
-            break; 
-
-            case "tr909hh":
-            case "tr909oh": //Hi-hat abierto y cerrado
-            fill (255, 255, 0);
-            rect(anim.x, anim.y, lerp(40, 0, progress), lerp(40, 0, progress)); //Dibuja un rectángulo que se encoge con el tiempo en la posición del hi-hat.
-            break;
-
-            default:
-                fill(255);
-                noFill();
-                rect(anim.x, anim.y, lerp(100, 0, progress), lerp(100, 0, progress)); //Dibuja un rectángulo genérico para otros tipos de animaciones.
-                break;
+            painter.activeAnimations.splice(i, 1); //Eliminar la animación de la lista de animaciones activas.
+            continue; // Saltar a la siguiente animación.
         }
-    }
+            switch (anim.type) {
+                case "tr909bd": //Bombo
+                fill (255, 0, 80);
+                noStroke();
+                circle(width / 2, height / 2, lerp(50, 300, progress)); //Dibuja un círculo que crece con el tiempo.
+                break;
+                
+                case "tr909sd": //Caja
+                fill (0, 200, 255);
+                rectMode(CENTER); 
+                rect(width / 2, height / 2, lerp(width, 0, progress), 50); //Dibuja un rectángulo que se encoge con el tiempo.
+                break; 
+                
+                case "tr909hh":
+                    case "tr909oh": //Hi-hat abierto y cerrado
+                    fill (255, 255, 0);
+                    rect(anim.x, anim.y, lerp(40, 0, progress), lerp(40, 0, progress)); //Dibuja un rectángulo que se encoge con el tiempo en la posición del hi-hat.
+                    break;
+                    
+                default:
+                    fill(255);
+                    noFill();
+                    rect(anim.x, anim.y, lerp(100, 0, progress), lerp(100, 0, progress)); //Dibuja un rectángulo genérico para otros tipos de animaciones.
+                    break;
+                }
+            }
+        }
+        
+        function windowResized() {
+            resizeCanvas(windowWidth, windowHeight);
+        } 
+       
 
-}
+            
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-} 
+
+
